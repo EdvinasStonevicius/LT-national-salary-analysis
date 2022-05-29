2b44cf04af5f3bbbc40e5ffcd785bc1334282d33
EdvinasStonevicius <102312862+EdvinasStonevicius@users.noreply.github.com>
2022-05-29 23:15:11 +0300

    Initial upload 2022-05-29
    Update 02_tidy.sql

---
+++ SQL/02_tidy.sql
@@ -0,0 +1,12 @@
+USE salary_lt;
+
+-- Remove leading X in nace field
+UPDATE employees
+SET nace = TRIM(LEADING 'X' FROM nace)
+WHERE length(nace)>1 AND key_e> 0 ; -- Table key  used in statment to overrun safe mode
+
+-- Remove redundant fields
+ALTER TABLE employees
+DROP COLUMN _type, 
+DROP COLUMN _id, 
+DROP COLUMN _revision;

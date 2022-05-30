b8c2cf85f5f6dca006e607a557dd62b92c295cac
EdvinasStonevicius <102312862+EdvinasStonevicius@users.noreply.github.com>
2022-05-30 22:40:07 +0300

    Update 02_tidy.sql

--- SQL/02_tidy.sql
+++ SQL/02_tidy.sql
@@ -3,7 +3,7 @@ USE salary_lt;
 -- Remove leading X in nace field
 UPDATE employees
 SET nace = TRIM(LEADING 'X' FROM nace)
-WHERE length(nace)>1 AND key_e> 0 ; -- Table key  used in statment to overrun safe mode
+WHERE length(nace)>1 AND key_e> 0 ; -- Table key  used in statment to bypass safe mode
 
 
 -- Remove redundant fields

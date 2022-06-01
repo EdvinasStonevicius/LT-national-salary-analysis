f33960623c18859f91e529158334bf1cdbeb7a05
EdvinasStonevicius <102312862+EdvinasStonevicius@users.noreply.github.com>
2022-06-01 16:42:04 +0300

    Fixed currency for 2014

--- SQL/02_tidy.sql
+++ SQL/02_tidy.sql
@@ -5,6 +5,17 @@ UPDATE employees
 SET nace = TRIM(LEADING 'X' FROM nace)
 WHERE length(nace)>1 AND key_e> 0 ; -- Table key used in statment to bypass safe mode
 
+-- Change currency for salary in 2014 from LTL to EUR 
+-- (Lithuania adopted the euro on 1 January 2015). 
+UPDATE employees
+SET gross_salary = round(gross_salary / 3.4528, 2),
+	bonuses = round(bonuses / 3.4528, 2),
+    gross_salary_oct = round(gross_salary_oct / 3.4528, 2),
+    bonuses_oct = round(bonuses_oct / 3.4528, 2),
+    bonuses_add_oct = round(bonuses_add_oct / 3.4528, 2),
+    hourly_rate = round(hourly_rate / 3.4528, 2) 
+WHERE year = 2014 AND key_e> 0 ; -- Table key used in statment to bypass safe mode
+
 -- Add total salary and total october salary as a sum of gross and bounuses. 
 -- Stored, because updates are unlikely.
 
--- SQL/03_Analysis.sql
+++ SQL/03_Analysis.sql
@@ -1,9 +1,35 @@
 USE salary_lt;
 
 -- Number of employees of each profession
-SELECT e.lpk, p.profession, count(lpk) as lpk_count
+SELECT 	e.lpk,
+		p.profession,
+		count(lpk) as lpk_count
 FROM employees e
-INNER JOIN lpk_profession p USING(lpk) 
+JOIN lpk_profession p USING(lpk) 
 GROUP BY lpk
 ORDER BY lpk_count DESC;
 
+-- Average salary of each profession in 2018
+SELECT 	e.lpk,
+		p.profession,
+        round(avg(total), 0) as avg_total,
+        round(avg(total_oct), 0) as avg_total_oct,
+        round(avg(hourly_rate), 2) as avg_hourly_rate
+FROM employees e
+JOIN lpk_profession p USING(lpk) 
+WHERE year = 2018
+GROUP BY lpk
+ORDER BY avg_hourly_rate DESC;
+
+/* padaryti kad būtų skirtingi metai šalia
+-- Average hourly rate of profession in 2014 and 2018
+SELECT 	p.lpk,
+		p.profession,
+        round(avg(e14.hourly_rate), 2) as hourly_rate_2014,
+        round(avg(e18.hourly_rate), 2) as hourly_rate_2018
+FROM employees e14
+JOIN employees e18 USING(lpk)
+JOIN lpk_profession p USING(lpk) 
+GROUP BY lpk
+ORDER BY hourly_rate_2014 DESC;
+*/
